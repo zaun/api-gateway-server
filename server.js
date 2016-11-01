ea450839bfaf8ac7c7049f65b7fc5a4b49525463
Alex Sue <asue@teampraxis.com>
2016-11-01 11:22:51 -1000

    Add the CORS headers for the options

--- server.js
+++ server.js
@@ -14,6 +14,33 @@ if (process.argv.length <= 2) {
 
 var app = express();
 
+app.use(function (req, res, next) {
+  if (req.method === 'OPTIONS') {
+    var ALLOWED_HEADERS = [
+      'Accept',
+      'Accept-Encoding',
+      'Accept-Version',
+      'Allow',
+      'Authorization',
+      'Cache-Control',
+      'Content-Type',
+      'Origin',
+      'Pragma',
+      'Set-Cookie',
+      'X-Prototype-Version',
+      'X-Requested-With',
+      'X-Sagely-Client'
+    ];
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', ALLOWED_HEADERS.join(','));
+    res.header('Access-Control-Expose-Headers', 'X-Server-Version');
+    res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+  }
+
+  next();
+});
+
 var options = {
   controllers: './controllers',
   useStubs: true
